9da66dd133fb66f88e515c27413f8890b81ed53b
Bryan Leanoe <bryan.leano@gmail.com>
2021-10-12 14:19:10 -0600

    add useState

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App() {
+  const [status, setStatus] = useState('Not Delivered');
+  return (
+    <div>
+      <h1>The package is: {status}.</h1>
+      <button onClick={() => setStatus('Delivered')}>Deliver</button>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +19,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
